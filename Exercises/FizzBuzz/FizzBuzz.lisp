b8befb1390e7302cfff8dd63db284bd585cd3dc5
Flávio Silva <55815265+201flaviosilva@users.noreply.github.com>
2023-03-02 17:17:19 +0000

    update

--- Exercises/FizzBuzz/FizzBuzz.lisp
+++ Exercises/FizzBuzz/FizzBuzz.lisp
@@ -1,14 +1,14 @@
-(write-line "-- Fizz Buzz --") ; prints the header of the program.
+(write-line "-- Fizz Buzz --")
 
 (format t "Number: ")
-(let ((userNumber (read))) ; reads a number from the user and binds it to the variable userNumber.
+(let ((userNumber (read))) ; reads a number from the user and binds it to the variable userNumber
   (setq output "") ; initializes an new empty string variable
-  (when (zerop (mod userNumber 3)) ; checks if the userNumber is divisible by 3 and concatenates "Fizz" to output if it is.
+  (when (zerop (mod userNumber 3)) ; checks if the userNumber is divisible by 3 and add "Fizz"
     (setq output (concatenate 'string output "Fizz"))
     )
   (when (zerop (mod userNumber 5)) ; same but for 5 and "Buzz"
     (setq output (concatenate 'string output "Buzz"))
     )
-  (unless output ; checks if output is still an empty string.
-    (setq output userNumber)) ; assigns the userNumber value to output if it is not divisible by 3 or 5.
-  (format t "~a" output)) ; prints the final value of output.
+  (unless output ; checks if it still an empty string
+    (setq output userNumber)) ; assigns the userNumber value to output if it is not divisible by 3 or 5
+  (format t "~a" output)) ; prints the final value of output
